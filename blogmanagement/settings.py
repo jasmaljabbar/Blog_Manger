da66f4e171899dfc2650574f233b1415a4069c69
jasmal jabbar <jasmaljabbar24@gmil.com>
2024-11-08 17:39:26 +0530

    change token time

--- blogmanagement/settings.py
+++ blogmanagement/settings.py
@@ -158,7 +158,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
