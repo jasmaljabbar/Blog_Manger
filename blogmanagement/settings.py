5af0a575eb0397abb0ddd656f37c830c09a2a8be
jasmal jabbar <jasmaljabbar24@gmil.com>
2024-11-08 15:31:09 +0530

    change db for test

--- blogmanagement/settings.py
+++ blogmanagement/settings.py
@@ -97,12 +97,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',  
-        'NAME': env('DATABASE_NAME'),
-        'USER': env('DATABASE_USER'),
-        'PASSWORD': env('DATABASE_PASSWORD'),
-        'HOST': env('DATABASE_HOST'),
-        'PORT': env('DATABASE_PORT'),  
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
