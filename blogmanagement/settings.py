efad7dae1f3fbd844a294ab83d44bfde35161ec7
jasmal jabbar <jasmaljabbar24@gmil.com>
2024-11-08 16:42:11 +0530

    added docker file

--- blogmanagement/settings.py
+++ blogmanagement/settings.py
@@ -97,8 +97,12 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),  
     }
 }
 
